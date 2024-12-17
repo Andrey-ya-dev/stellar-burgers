1f24f4d0d25a22c1f03e1f03eddcdb604878bc4e
Andrey Ignashov <maylo7771@yandex.ru>
2024-12-17 21:31:38 +0300

    Добавлен и подключен react-router,добавлены маршруты, добавлен ProtectedRoute

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,112 @@
-import { ConstructorPage } from '@pages';
+import {
+  ConstructorPage,
+  Feed,
+  ForgotPassword,
+  Login,
+  NotFound404,
+  Profile,
+  ProfileOrders,
+  Register,
+  ResetPassword
+} from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
 
-import { AppHeader } from '@components';
+import { AppHeader, IngredientDetails, Modal, OrderInfo } from '@components';
+import { Route, Routes } from 'react-router-dom';
+import { ReactNode } from 'react';
+
+type TProtectedRouter = {
+  children: ReactNode;
+};
+const ProtectedRouter = ({ children }: TProtectedRouter) => {
+  return children;
+};
 
 const App = () => (
   <div className={styles.app}>
     <AppHeader />
-    <ConstructorPage />
+    <Routes>
+      <Route path='/' element={<ConstructorPage />} />
+      <Route path='/feed' element={<Feed />} />
+      <Route
+        path='/login'
+        element={
+          <ProtectedRouter>
+            <Login />
+          </ProtectedRouter>
+        }
+      />
+      <Route
+        path='/register'
+        element={
+          <ProtectedRouter>
+            <Register />
+          </ProtectedRouter>
+        }
+      />
+      <Route
+        path='/forgot-password'
+        element={
+          <ProtectedRouter>
+            <ForgotPassword />
+          </ProtectedRouter>
+        }
+      />
+      <Route
+        path='/reset-password'
+        element={
+          <ProtectedRouter>
+            <ResetPassword />
+          </ProtectedRouter>
+        }
+      />
+      <Route
+        path='/profile'
+        element={
+          <ProtectedRouter>
+            <Profile />
+          </ProtectedRouter>
+        }
+      />
+      <Route
+        path='/profile/orders'
+        element={
+          <ProtectedRouter>
+            <ProfileOrders />
+          </ProtectedRouter>
+        }
+      />
+      <Route path='*' element={<NotFound404 />} />
+
+      {/* MODALKI */}
+      <Route
+        path='/feed/:number'
+        element={
+          <Modal title='' onClose={() => {}}>
+            <OrderInfo />
+          </Modal>
+        }
+      />
+      <Route
+        path='/ingredients/:id'
+        element={
+          <Modal title='' onClose={() => {}}>
+            <IngredientDetails />
+          </Modal>
+        }
+      />
+      <Route
+        path='/profile/orders/:number'
+        element={
+          <Modal title='' onClose={() => {}}>
+            <ProtectedRouter>
+              <OrderInfo />
+            </ProtectedRouter>
+          </Modal>
+        }
+      />
+    </Routes>
   </div>
 );
 
