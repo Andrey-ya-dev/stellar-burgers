35976bb9d778e28aa053ce223156283dbf79d95c
Andrey Ignashov <maylo7771@yandex.ru>
2024-12-18 23:03:53 +0300

    Добавлено отображение ингредиентов при клике в модальном окне

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -13,14 +13,20 @@ import '../../index.css';
 import styles from './app.module.css';
 
 import { AppHeader, IngredientDetails, Modal, OrderInfo } from '@components';
-import { Navigate, Route, Routes } from 'react-router-dom';
+import {
+  Navigate,
+  Route,
+  Routes,
+  useLocation,
+  useNavigate
+} from 'react-router-dom';
 import { ReactNode } from 'react';
 
 type TProtectedRouter = {
   children: ReactNode;
 };
 const ProtectedRouter = ({ children }: TProtectedRouter) => {
-  const onlyUnAuth = false;
+  const onlyUnAuth = true;
 
   if (!onlyUnAuth) {
     return <Navigate replace to='/login' />;
@@ -29,91 +35,105 @@ const ProtectedRouter = ({ children }: TProtectedRouter) => {
   return children;
 };
 
-const App = () => (
-  <div className={styles.app}>
-    <AppHeader />
-    <Routes>
-      <Route path='/' element={<ConstructorPage />} />
-      <Route path='/feed' element={<Feed />} />
-      <Route
-        path='/login'
-        element={
-          <ProtectedRouter>
-            <Login />
-          </ProtectedRouter>
-        }
-      />
-      <Route
-        path='/register'
-        element={
-          <ProtectedRouter>
-            <Register />
-          </ProtectedRouter>
-        }
-      />
-      <Route
-        path='/forgot-password'
-        element={
-          <ProtectedRouter>
-            <ForgotPassword />
-          </ProtectedRouter>
-        }
-      />
-      <Route
-        path='/reset-password'
-        element={
-          <ProtectedRouter>
-            <ResetPassword />
-          </ProtectedRouter>
-        }
-      />
-      <Route
-        path='/profile'
-        element={
-          <ProtectedRouter>
-            <Profile />
-          </ProtectedRouter>
-        }
-      />
-      <Route
-        path='/profile/orders'
-        element={
-          <ProtectedRouter>
-            <ProfileOrders />
-          </ProtectedRouter>
-        }
-      />
-      <Route path='*' element={<NotFound404 />} />
+const App = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const background = location.state?.background;
 
-      {/* MODALKI */}
-      <Route
-        path='/feed/:number'
-        element={
-          <Modal title='' onClose={() => {}}>
-            <OrderInfo />
-          </Modal>
-        }
-      />
-      <Route
-        path='/ingredients/:id'
-        element={
-          <Modal title='' onClose={() => {}}>
-            <IngredientDetails />
-          </Modal>
-        }
-      />
-      <Route
-        path='/profile/orders/:number'
-        element={
-          <Modal title='' onClose={() => {}}>
+  return (
+    <div className={styles.app}>
+      <AppHeader />
+      <Routes location={background || location}>
+        <Route path='/' element={<ConstructorPage />} />
+        <Route path='/feed' element={<Feed />} />
+        <Route
+          path='/login'
+          element={
             <ProtectedRouter>
-              <OrderInfo />
+              <Login />
             </ProtectedRouter>
-          </Modal>
-        }
-      />
-    </Routes>
-  </div>
-);
+          }
+        />
+        <Route
+          path='/register'
+          element={
+            <ProtectedRouter>
+              <Register />
+            </ProtectedRouter>
+          }
+        />
+        <Route
+          path='/forgot-password'
+          element={
+            <ProtectedRouter>
+              <ForgotPassword />
+            </ProtectedRouter>
+          }
+        />
+        <Route
+          path='/reset-password'
+          element={
+            <ProtectedRouter>
+              <ResetPassword />
+            </ProtectedRouter>
+          }
+        />
+        <Route
+          path='/profile'
+          element={
+            <ProtectedRouter>
+              <Profile />
+            </ProtectedRouter>
+          }
+        />
+        <Route
+          path='/profile/orders'
+          element={
+            <ProtectedRouter>
+              <ProfileOrders />
+            </ProtectedRouter>
+          }
+        />
+        <Route path='*' element={<NotFound404 />} />
+      </Routes>
+      {/* MODALKI */}
+      {background && (
+        <Routes>
+          <Route
+            path='/feed/:number'
+            element={
+              <Modal title='' onClose={() => {}}>
+                <OrderInfo />
+              </Modal>
+            }
+          />
+          <Route
+            path='/ingredients/:id'
+            element={
+              <Modal
+                title='Детали ингредиента'
+                onClose={() => {
+                  navigate(-1);
+                }}
+              >
+                <IngredientDetails />
+              </Modal>
+            }
+          />
+          <Route
+            path='/profile/orders/:number'
+            element={
+              <Modal title='' onClose={() => {}}>
+                <ProtectedRouter>
+                  <OrderInfo />
+                </ProtectedRouter>
+              </Modal>
+            }
+          />
+        </Routes>
+      )}
+    </div>
+  );
+};
 
 export default App;
--- src/components/ingredient-details/ingredient-details.tsx
+++ src/components/ingredient-details/ingredient-details.tsx
@@ -1,10 +1,17 @@
 import { FC } from 'react';
 import { Preloader } from '../ui/preloader';
 import { IngredientDetailsUI } from '../ui/ingredient-details';
+import { useSelector } from '../../services/store';
+import { getIngredientsData } from '../../services/indgredientSlice/ingredientSlice';
+import { useParams } from 'react-router-dom';
 
 export const IngredientDetails: FC = () => {
-  /** TODO: взять переменную из стора */
-  const ingredientData = null;
+  /** TODO: взять переменную из стора 
+  const ingredientData = null;*/
+  const { id: ingredientId } = useParams();
+  const ingredientData = useSelector(getIngredientsData).find(
+    (item) => item._id === ingredientId
+  );
 
   if (!ingredientData) {
     return <Preloader />;
